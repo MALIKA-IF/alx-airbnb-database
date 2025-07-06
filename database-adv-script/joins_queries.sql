5a5d2d1ee50e92111ca1da8b9cdb729ba2b771f0
Malika Edaif <edmalika957@gmail.com>
2025-07-06 14:23:30 +0100

    create joins queries

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -3,3 +3,5 @@ USE AIRBNB_DATA;
 SELECT * FROM booking INNER JOIN user ON booking.booking_id = booking.user_id;
 
 SELECT * FROM property LEFT JOIN review ON review.property_id=review.review_id ORDER BY property.name;
+
+SELECT * FROM booking FULL OUTER JOIN User ON User.user_id=booking.booking_id;
