0588e4389eda5a9acd4509c9cc00e2542f4cbf1e
Malika Edaif <edmalika957@gmail.com>
2025-07-14 20:27:14 +0100

    create two sql commande in aggregations

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -2,4 +2,4 @@ USE AIRBNB_DATA;
 
 SELECT COUNT(booking_id) FROM booking GROUP BY user_id;
 
-SELECT RANK() OVER (ORDER BY property_id) AS booking_id_row_number FROM booking;
+SELECT RANK() OVER (ORDER BY property_id) AS booking_id_ROW_NUMBER FROM booking;
